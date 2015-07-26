2fce460d3d0115f9672bda47661b246ff63faa7b
Marin Jelev <marin.jelev92@gmail.com>
2015-07-26 14:23:34 +0300

    Finished VariablesDataExpressionsAndConstants HW

---
+++ High Quality Code/VariablesDataExpressionsAndConstants/FigureRotation/RotatedFigure.cs
@@ -0,0 +1,65 @@
+﻿namespace FigureRotation
+{
+    using System;
+
+    public class RotatedFigure
+    {
+        private double width;
+        private double height;
+
+        public RotatedFigure(double newWidth, double newHeight)
+        {
+            this.Width = newWidth;
+            this.Height = newHeight;
+        }
+
+        public double Width 
+        {
+            get
+            {
+                return this.width;
+            }
+
+            set
+            {
+                this.ValidateParameter(value);
+                this.width = value;
+            }
+        }
+
+        public double Height
+        {
+            get
+            {
+                return this.height;
+            }
+
+            set
+            {
+                this.ValidateParameter(value);
+                this.height = value;
+            }
+        }
+
+        public static RotatedFigure GetRotatedSize(RotatedFigure figure, double angleOfRotation)
+        {
+            var cosOfAngle = Math.Cos(angleOfRotation);
+            var sinOfAngle = Math.Sin(angleOfRotation);
+
+            var newWidth = (Math.Abs(cosOfAngle) * figure.width) + (Math.Abs(sinOfAngle) * figure.height);
+            var newHeight = (Math.Abs(sinOfAngle) * figure.width) + (Math.Abs(cosOfAngle) * figure.height);
+
+            RotatedFigure rotatedFigure = new RotatedFigure(newWidth, newHeight);
+
+            return rotatedFigure;
+        }
+
+        private void ValidateParameter(double parameter)
+        {
+            if (parameter <= 0)
+            {
+                throw new ArgumentException("Size must be a positive number.");
+            }
+        }
+    }
+}
---
+++ High Quality Code/VariablesDataExpressionsAndConstants/Statistics/ArrayMethods.cs
@@ -0,0 +1,56 @@
+﻿namespace Statistics
+{
+    public class ArrayMethods
+    {
+        public void PrintArrayStatistics(double[] array)
+        {
+            this.PrintMax(array);
+            this.PrintMin(array);
+            this.PrintAverage(array);
+        }
+
+        private double PrintMax(double[] array)
+        {
+            double maxElement = double.MinValue;
+
+            for (int index = 0; index < array.Length; index++)
+            {
+                if (array[index] > maxElement)
+                {
+                    maxElement = array[index];
+                }
+            }
+
+            return maxElement;
+        }
+
+        private double PrintMin(double[] array)
+        {
+            double minElement = double.MaxValue;
+
+            for (int index = 0; index < array.Length; index++)
+            {
+                if (array[index] < minElement)
+                {
+                    minElement = array[index];
+                }
+            }
+
+            return minElement;
+        }
+
+        private double PrintAverage(double[] array)
+        {
+            double sum = 0;
+
+            for (int index = 0; index < array.Length; index++)
+            {
+                sum = array[index];
+            }
+
+            double average;
+            average = sum / array.Length;
+            return average;
+        }
+    }
+}
