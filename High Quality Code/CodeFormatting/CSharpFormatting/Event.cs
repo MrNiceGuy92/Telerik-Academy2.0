ebacd0a3c13e0eaf343a142d9779b7c6548c4fa3
Marin Jelev <marin.jelev92@gmail.com>
2015-07-26 16:04:30 +0300

    Code Formatting fix

---
+++ High Quality Code/CodeFormatting/CSharpFormatting/Event.cs
@@ -0,0 +1,94 @@
+﻿namespace CSharpFormatting
+{
+    using System;
+    using System.Text;
+
+    public class Event : IComparable
+    {
+        private DateTime date;
+        private string title;
+        private string location;
+
+        public Event(DateTime date, string title, string location)
+        {
+            this.Date = date;
+            this.Title = title;
+            this.Location = location;
+        }
+
+        public DateTime Date { get; set; }
+
+        public string Title
+        {
+            get
+            {
+                return this.title;
+            }
+
+            set
+            {
+                if (value.Length < 10)
+                {
+                    throw new ArgumentException("Invalid length of title.");
+                }
+
+                this.title = value;
+            }
+        }
+
+        public string Location
+        {
+            get
+            {
+                return this.location;
+            }
+
+            set
+            {
+                if (value == null)
+                {
+                    throw new ArgumentNullException("Location cannot be null.");
+                }
+
+                this.location = value;
+            }
+        }
+
+        public int CompareTo(object inputObject)
+        {
+            Event otherDate = inputObject as Event;
+
+            int compareByDate = this.date.CompareTo(otherDate.date);
+            int compareByTitle = this.title.CompareTo(otherDate.title);
+            int compareByLocation = this.location.CompareTo(otherDate.location);
+
+            if (compareByDate == 0 && compareByTitle != 0)
+            {
+                return compareByLocation;
+            }
+            else if (compareByDate == 0 && compareByTitle == 0)
+            {
+                return compareByTitle;
+            }
+            else
+            {
+                return compareByDate;
+            }
+        }
+
+        public override string ToString()
+        {
+            StringBuilder toString = new StringBuilder();
+
+            toString.Append(this.date.ToString("yyyy-MM-ddTHH:mm:ss"));
+            toString.Append(" | " + this.title);
+
+            if (this.location != null && this.location != string.Empty)
+            {
+                toString.Append(" | " + this.location);
+            }
+
+            return toString.ToString();
+        }
+    }
+}
---
+++ High Quality Code/CodeFormatting/CSharpFormatting/EventHandler.cs
@@ -0,0 +1,63 @@
+﻿namespace CSharpFormatting
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using Wintellect.PowerCollections;
+
+    public class EventHolder
+    {
+        private MultiDictionary<string, Event> comparedByTitleMultiDictionary = new MultiDictionary<string, Event>(true);
+        private OrderedBag<Event> comparedByDateOrderedBag = new OrderedBag<Event>();
+
+        public void AddEvent(
+                             DateTime date,
+                             string title,
+                             string location)
+        {
+            Event newEvent = new Event(date, title, location);
+            this.comparedByTitleMultiDictionary.Add(title.ToLower(), newEvent);
+            this.comparedByDateOrderedBag.Add(newEvent);
+            EventExecuter.Messages.EventAdded();
+        }
+
+        public void DeleteEvents(string titleToDelete)
+        {
+            string title = titleToDelete.ToLower();
+            int removed = 0;
+
+            foreach (var eventToRemove in this.comparedByTitleMultiDictionary[title])
+            {
+                removed++;
+                this.comparedByDateOrderedBag.Remove(eventToRemove);
+            }
+
+            this.comparedByTitleMultiDictionary.Remove(title);
+            EventExecuter.Messages.EventDeleted(removed);
+        }
+
+        public void ListEvents(DateTime date, int count)
+        {
+            OrderedBag<Event>.View eventsToShow = this.comparedByDateOrderedBag.RangeFrom(new Event(date, string.Empty, string.Empty), true);
+            int showed = 0;
+
+            foreach (var eventToShow in eventsToShow)
+            {
+                if (showed == count)
+                {
+                    break;
+                }
+
+                Messages.PrintEvent(eventToShow);
+                showed++;
+            }
+
+            if (showed == 0)
+            {
+                EventExecuter.Messages.NoEventsFound();
+            }
+        }
+    }
+}
